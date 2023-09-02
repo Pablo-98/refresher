ae939648c3c484fa315e2510cb28b652b20f4204
Pablo Hernandez <pablo@Pablos-Air.lan1>
2023-09-01 20:36:00 -0600

    part II done

--- read.java
+++ read.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 
 public class read {
 
+
+
     /*
      1. [data type] arrayName = new {data type} [array size]
      2. Math.random() * ____
@@ -24,48 +26,82 @@ public class read {
      10. typecast. Example: int() DoubleVar
      */
 
+     // part II
+
+/*Write a program called RemedialActivity.java, where you implement the following instructions
+1. In the main, reads a file called numbers.txt. This file contains numbers, one per line. The
+first line in the file represents the total number of values in the file.
+2. Read the file and store the values into an array of ints.
+3. Print the elements of the array by calling the method print()
+4. Calculate the sum of the array by calling the method findSum()
+5. Calculate the average of the array by calling the method findAVG(). 
+*/
+    public static void print(int[] a) {
+        for (int i = 0; i <a.length; i++){
+
+            System.out.println(a[i]);
+        }
+    }
+
+    public static int findSum(int[] a){
+        int sum=0;
+
+        for( int i = 0; i < a.length; i++){
+            sum = sum + a[i];
+        }
+        return sum;
+    }
+
+    public static double findAVG (int[] a ) {
+         double sum=0;
+         double avg = 0;
+
+        for( int i = 0; i < a.length; i++){
+            sum = sum + a[i];
+        }
+        return sum/a.length;
+    }
+
     public static void main(String[] args) throws IOException {
 
         Scanner input = new Scanner(new File("num.txt"));
 
         // this array will hold the strings we receive from the num.txt file
-        String[] array = new String[10];
+        String[] stringArray = new String[10];
+        int[] intArray = new int[10];
 
     // my main concern at this moment (9/01/23 1:26pm ) is that I cant find a way to skip over the first element in the num.txt file. In our case its the number 10
     // we need to not have 10 in our final array
   
-/* method 1 ( I think I'm on the right path )  this will store ALL the values from the num.txt file.
-        for ( int k = 1; k < array.length ; k++) {
-           
-                String line = input.nextLine();
-            
-                array[k] = line;
-            }
-          */
-          /*
-           method 1 ^^ output: 
-           null
-           10
-           8
-           12
-           9
-           11
-           2
-           24
-           32
-           8
-           */
 
-        for (int k = 1; k< array.length; k++) {
-            String line = input.nextLine();
-            array[k] = line; 
-        }
-        
-// simple for loop to iterate and display every element in our string array 
-        for ( int j = 0; j < array.length; j++) {
 
-            System.out.println(array[j]);
+   for(int i = 0; i <stringArray.length; i++){
+    if (input.hasNext()) {
+        String line = input.nextLine();
+        stringArray [i] = line;
+    }
+
+   }
+
+    
+        for ( int j = 0; j < stringArray.length; j++) {
+           String lineTwo = stringArray [j];
+           int myInt = Integer.parseInt(lineTwo); 
+            intArray[j] = myInt;
+            System.out.println("my intArray now :" + intArray[j]);
         }
+
+        // at this point I have a int array 
+         System.out.println("-------------- print method ----------");
+
+        print (intArray);
+
+        System.out.println("-------------- findAVG method  ----------");
+        System.out.println(findSum(intArray));
+
+        System.out.println("-------------- findAVG method  ----------");
+        System.out.println(findAVG(intArray));
+
 
        
 
