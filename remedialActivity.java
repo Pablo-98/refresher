09c539f627204f5503f36690474bd8903dc55d0b
Pablo Hernandez <pablo@Pablos-MacBook-Air.local>
2023-09-02 15:37:20 -0600

    updated part III

--- remedialActivity.java
+++ remedialActivity.java
@@ -5,9 +5,10 @@ public class remedialActivity {
 // lab pair: 
 //Pablo Hernandez 
 // John Guajardo 
-
+// You are awesome professor Karichev!!!
 
     /*
+    Part I
      1. [data type] arrayName = new {data type} [array size]
      2. Math.random() * ____
      3. int array = new int [10];
@@ -28,23 +29,25 @@ public class remedialActivity {
      10. typecast. Example: int() DoubleVar
      */
 
-     // part II
-
-/*Write a program called RemedialActivity.java, where you implement the following instructions
-1. In the main, reads a file called numbers.txt. This file contains numbers, one per line. The
-first line in the file represents the total number of values in the file.
-2. Read the file and store the values into an array of ints.
-3. Print the elements of the array by calling the method print()
-4. Calculate the sum of the array by calling the method findSum()
-5. Calculate the average of the array by calling the method findAVG(). 
-*/
+   
+/**
+ This method will iterate through an array given and print out all values of that array 
+ * @param a - integer array provided by user 
+ *
+ */
     public static void print(int[] a) {
         for (int i = 0; i <a.length; i++){
 
             System.out.println(a[i]);
         }
     }
-
+/**
+ This method will iterate through an array given and 
+  store the value of each element in the array into a 
+  variable called sum. This sum variable will be returned 
+ * @param a - integer array provided by user 
+ * @return the sum of the all the values in the array
+ */
     public static int findSum(int[] a){
         int sum=0;
 
@@ -53,16 +56,35 @@ first line in the file represents the total number of values in the file.
         }
         return sum;
     }
-
+/**
+ This method will iterate through an array given and 
+  store the value of each element in the array into a 
+  variable called sum. After the sum variable is determined, 
+  the method will divide that value by the length of the array 
+  and return that value as the average
+ * @param a - integer array provided by user 
+ * @return the average value of the of the array 
+ */
     public static double findAVG (int[] a ) {
          double sum=0;
-         double avg = 0;
+       
 
         for( int i = 0; i < a.length; i++){
             sum = sum + a[i];
         }
         return sum/a.length;
     }
+
+/*
+Part II
+Write a program called RemedialActivity.java, where you implement the following instructions
+1. In the main, reads a file called numbers.txt. This file contains numbers, one per line. The
+first line in the file represents the total number of values in the file.
+2. Read the file and store the values into an array of ints.
+3. Print the elements of the array by calling the method print()
+4. Calculate the sum of the array by calling the method findSum()
+5. Calculate the average of the array by calling the method findAVG(). 
+*/
 
     public static void main(String[] args) throws IOException {
 
@@ -75,7 +97,7 @@ first line in the file represents the total number of values in the file.
  
   
 
-
+// initialize my string array 
    for(int i = 0; i <stringArray.length; i++){
     if (input.hasNext()) {
         String line = input.nextLine();
@@ -84,7 +106,7 @@ first line in the file represents the total number of values in the file.
 
    }
 
-    
+    // initialize my integer array 
         for ( int j = 0; j < stringArray.length; j++) {
            String lineTwo = stringArray [j];
            int myInt = Integer.parseInt(lineTwo); 
@@ -92,7 +114,7 @@ first line in the file represents the total number of values in the file.
             System.out.println("my intArray now :" + intArray[j]);
         }
 
-        // at this point I have a int array 
+       
          System.out.println("-------------- print method ----------");
 
         print (intArray);
@@ -102,6 +124,40 @@ first line in the file represents the total number of values in the file.
 
         System.out.println("-------------- findAVG method  ----------");
         System.out.println(findAVG(intArray));
+
+        /*
+        Part III
+          Now that you know how to read a file and populate the values of the file into an array.
+1. Write a file called ‘‘results.txt’’. The file must contain the following information
+• the values that are in the array in the following format: e1 +e2 +...+en
+where the e1 is the first number from the file, e2 is the second number from the file, etc
+• the sum:
+• the average:
+E.g., the file results.txt must have the following information
+3 + 7 + 7 + 3 + 10 + 1 + 1 + 1 + 3 + 9 = 45
+sum: 45
+average: 4.5
+
+         
+         */
+        System.out.println("-------------------part III-------------------");
+        PrintWriter myPrinter = new PrintWriter("results.txt");
+        for (int g=0; g <intArray.length; g++) {
+            if (g < 9){
+            myPrinter.print(intArray[g] + " + ");
+            }
+            else if (g==9){
+                myPrinter.print(intArray[g]);
+            }
+        }
+        myPrinter.println();
+        myPrinter.println("the sum " + findSum(intArray));
+        myPrinter.println("the average " + findAVG(intArray));
+        myPrinter.close();
+
+        
+
+
 
 
        
